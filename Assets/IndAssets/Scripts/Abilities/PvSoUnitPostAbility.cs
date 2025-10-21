db3446a1da5e9ecef32c28036a63d4027be4e2b2
Junda Kong <wqjiujiu@gmail.com>
2025-10-21 19:32:44 -0400

    Add fully post toggle in post ability

--- Assets/IndAssets/Scripts/Abilities/PvSoUnitPostAbility.cs
+++ Assets/IndAssets/Scripts/Abilities/PvSoUnitPostAbility.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using ProjectCI.CoreSystem.Runtime.TacticRpgTool.Concrete;
-using ProjectCI.CoreSystem.Runtime.TacticRpgTool.GridData;
 using ProjectCI.Utilities.Runtime.Events;
 using UnityEngine;
 
@@ -9,23 +8,41 @@ namespace ProjectCI.CoreSystem.Runtime.Abilities
     [CreateAssetMenu(fileName = "NewPostAbility", menuName = "ProjectCI Tools/Ability/Create Custom Post Ability", order = 1)]
     public class PvSoUnitPostAbility : PvSoUnitAbility
     {
+        [SerializeField] private bool isFullPost;
+        
         public override List<CombatingQueryContext> OnCombatingQueryListCreated(PvMnBattleGeneralUnit caster,
             PvMnBattleGeneralUnit victim, bool casterSpeedExceed, bool victimSpeedExceed)
         {
-            var baseResult =
+            var baseResults =
                 base.OnCombatingQueryListCreated(caster, victim, casterSpeedExceed, victimSpeedExceed);
 
-            if (baseResult.Count <= 1)
+            var resultsCount = baseResults.Count;
+            if (resultsCount <= 1)
             {
-                return baseResult;
+                return baseResults;
             }
 
-            var initiativeQuery = baseResult[0];
-            var counterQuery = baseResult[1];
-            baseResult[0] = counterQuery;
-            baseResult[1] = initiativeQuery;
+            var initiativeQuery = baseResults[0];
+            var counterQuery = baseResults[1];
+            baseResults[0] = counterQuery;
+            baseResults[1] = initiativeQuery;
 
-            return baseResult;
+            if (isFullPost && resultsCount > 2)
+            {
+                var toReorderResults = new Queue<CombatingQueryContext>();
+                for (var i = resultsCount - 1; i >= 2; i--)
+                {
+                    toReorderResults.Enqueue(baseResults[i]);
+                    baseResults.RemoveAt(i);
+                }
+
+                while (toReorderResults.TryDequeue(out var result))
+                {
+                    baseResults.Insert(1, result);
+                }
+            }
+
+            return baseResults;
         }
     }
 }
