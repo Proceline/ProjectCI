9a039e076966a643d8acc8eed01caa258f7fa032
Proceline <wqjiujiu@gmail.com>
2026-02-21 23:27:19 -0500

    Finish AudioManager, before build game and lighting

--- Assets/IndAssets/Scripts/TacticTool/Battleground/PvMnLevelCell.cs
+++ Assets/IndAssets/Scripts/TacticTool/Battleground/PvMnLevelCell.cs
@@ -1,3 +1,4 @@
+using ProjectCI.CoreSystem.Runtime.TacticRpgTool.Gameplay;
 using ProjectCI.CoreSystem.Runtime.TacticRpgTool.GridData;
 
 namespace ProjectCI.CoreSystem.Runtime.TacticRpgTool.Concrete
@@ -7,7 +8,31 @@ namespace ProjectCI.CoreSystem.Runtime.TacticRpgTool.Concrete
         protected override void Start()
         {
             base.Start();
-            
+
+            var objRenderer = GetRenderer();
+            if (objRenderer)
+            {
+                objRenderer.enabled = false;
+            }
+
+        }
+
+        public override void SetMaterial(CellState InCellState)
+        {
+            var objRenderer = GetRenderer();
+
+            if (InCellState == CellState.eNormal)
+            {
+                objRenderer.enabled = false;
+            }
+            else
+            {
+                if (!objRenderer.enabled)
+                {
+                    objRenderer.enabled = true;
+                }
+                base.SetMaterial(InCellState);
+            }
         }
     }
 }
