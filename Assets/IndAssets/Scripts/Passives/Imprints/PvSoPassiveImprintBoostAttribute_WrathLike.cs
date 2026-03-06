65946dd6643331342af125787e53cca0bd8f0113
Proceline <wqjiujiu@gmail.com>
2026-03-06 17:52:52 -0500

    Update PvSoPassiveImprintBoostAttribute_WrathLike.cs

--- Assets/IndAssets/Scripts/Passives/Imprints/PvSoPassiveImprintBoostAttribute_WrathLike.cs
+++ Assets/IndAssets/Scripts/Passives/Imprints/PvSoPassiveImprintBoostAttribute_WrathLike.cs
@@ -1,14 +1,10 @@
-﻿using ProjectCI.CoreSystem.Runtime.TacticRpgTool.Concrete;
-using ProjectCI.Utilities.Runtime.Modifiers;
+﻿using ProjectCI.Utilities.Runtime.Modifiers;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace IndAssets.Scripts.Passives.Imprints
 {
     /// <summary>
-    /// Only used for Attribute Modifier, thus, no general effect, only personal effect.
-    /// The events are designed to be used for PvSoModifierSupportObject, 
-    /// which will be added to the unit personally when the relic is settled, and removed when the relic is withdrawn.
+    /// Same logic as FE->Wrath in Aike's passive
     /// </summary>
     [CreateAssetMenu(fileName = "PvSoImprintWrathLike", menuName = "ProjectCI Imprints/PvSoPassiveImprintWrathLike", order = 1)]
     public class PvSoPassiveImprintBoostAttribute_WrathLike : PvSoPassiveImprintBoostAttribute
